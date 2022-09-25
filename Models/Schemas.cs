109eefb567533fedc4e6dff9f0b4d950b911239f
ImDoctorWho <48880085+ImDoctorWho@users.noreply.github.com>
2022-09-25 21:15:11 +0500

    Добавьте файлы проекта.

---
+++ Models/Schemas.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppJsonParser.Models
+{
+
+
+
+    public class Rootobject
+    {
+        [Newtonsoft.Json.JsonProperty("Clients")]
+        public Client[] Clients { get; set; }
+    }
+
+    public class Client
+    {
+        public string Ссылка { get; set; }
+        public string Наименование { get; set; }
+        public decimal Факт { get; set; }
+        public decimal Предъявлено { get; set; }
+        public decimal КоличествоЧасов { get; set; }
+        public Договорыконтрагента[] ДоговорыКонтрагента { get; set; }
+    }
+
+    public class Договорыконтрагента
+    {
+        public string Ссылка { get; set; }
+        public string Наименование { get; set; }
+        public decimal Факт { get; set; }
+        public decimal Предъявлено { get; set; }
+        public decimal КоличествоЧасов { get; set; }
+        public Участки[] Участки { get; set; }
+    }
+
+    public class Участки
+    {
+        public string Ссылка { get; set; }
+        public string Наименование { get; set; }
+        public decimal Факт { get; set; }
+        public decimal Предъявлено { get; set; }
+        public decimal КоличествоЧасов { get; set; }
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using ConsoleAppJsonParser.Models;
+
+namespace ConsoleAppJsonParser
+{
+    class Program
+    {
+        static void Main()
+        {
+            var path = Path.GetFullPath("C:\\Users\\docto\\source\\repos\\ConsoleAppJsonParser\\code 1.json");
+            var json = File.ReadAllText(path);
+            var client = Newtonsoft.Json.JsonConvert.DeserializeObject<Models.Rootobject>(json);
+
+           
+            for (int i = 0; i < 22; i++)
+            {
+                try
+                {
+                    string nameClient = client.Clients[i].Наименование;
+                    double numOfHours = (double) client.Clients[i].КоличествоЧасов;
+                    double numOfHoursSysAd = (double) client.Clients[i].ДоговорыКонтрагента[0].Участки[2].КоличествоЧасов;
+
+
+                    if (numOfHours > 2.0 & numOfHoursSysAd > 0.5)
+                    {
+                        string output = String.Format("Наименование - {0}; Количество Часов - {1}; Кол. Часов \"Системное администрировани\" - {2};",
+                                                       nameClient, numOfHours, numOfHoursSysAd);
+                        Console.WriteLine(output);
+                    }
+                }
+                catch
+                {
+                    
+                }
+                     
+            }
+            
+            
+        }
+    }
+}
